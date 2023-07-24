cd9e722d684f790e26ca3aaaca7f8ff32d89ae04
Julien Laurenceau <julien.laurenceau@pepitedata.com>
2023-07-24 17:59:23 +0200

    update for kube 1.24

--- util/kubectl_sa.sh
+++ util/kubectl_sa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 usage () {
-    echo "Usage: $0 [-n spark -s admin-sa]"
+    echo "Usage: $0 [-n spark -s admin-sa -t admintoken-secret)]"
     echo "Examples:"
     echo "# namespace name:"
     echo " $0 -n spark"
@@ -15,6 +15,8 @@ while getopts "n:s:h" ARGOPTS ; do
         n) ns=$OPTARG
             ;;
         s) sa=$OPTARG
+            ;;
+        t) tokenname=$OPTARG
             ;;
         h) usage; exit;
             ;;
@@ -31,22 +33,39 @@ if [[ -z $ns || -z $sa ]]; then
 fi
 
 cacrt=tmp-kubectl_sa-cacert.pem
-rm -f ${cacrt} 2>/dev/null
 cluster=satest-cluster
 ctx=satest-context
 endpoint=https://kubernetes.default.svc:443
-endpoint=`grep -B 1 "name: $(kubectx -c)" ~/.kube/config | grep server | awk '{print $NF}'`
+endpoint=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
 echo "endpoint : $endpoint"
 if [[ -z $endpoint ]]; then
     echo "ERROR ! endpoint is empty"
     exit 9
 fi
-# kubectx $ctx=$(kubectx -c)
+# kubectl ctx $ctx=$(kubectx -c)
 
 kubectl config delete-context ${ctx}
 kubectl config delete-cluster ${cluster}
+set -e
 
-tokenname=`kubectl -n ${ns} get serviceaccount/${sa} -o jsonpath='{.secrets[0].name}'`
+minVer=$(kubectl version -o json | jq '.serverVersion.minor' | tr -d '"')
+
+# In the K8s version before 1.24, every time we would create a service account, a non-expiring secret token (Mountable secrets & Tokens) was created by default. However, from version 1.24 onwards, it was disbanded and no secret token is created by default when we create a service account. However, we can create it when need be.
+if [[ $minVer -le 23 ]]; then
+    tokenname=`kubectl -n ${ns} get serviceaccount/${sa} -o jsonpath='{.secrets[0].name}'`
+elif [[ -z $tokenname ]]; then
+    kubectl -n ${ns} create -f - <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ${sa}-token
+  annotations:
+    kubernetes.io/service-account.name: ${sa}
+type: kubernetes.io/service-account-token
+EOF
+    tokenname=${sa}-token
+fi
+
 token=`kubectl -n ${ns} get secret ${tokenname} -o jsonpath='{.data.token}'| base64 --decode`
 kubectl -n ${ns} get secret ${tokenname} -o jsonpath='{.data.ca\.crt}' | base64 --decode > ${cacrt}
 cat ${cacrt} | grep CERTIFICATE
@@ -54,7 +73,8 @@ kubectl config set-cluster ${cluster} --server=${endpoint}
 kubectl config set-credentials ${sa} --token=$token
 kubectl config set-context ${ctx} --user=${sa} --cluster=${cluster}
 kubectl config set-cluster ${cluster} --embed-certs --certificate-authority <(cat ${cacrt})
-# kubectl config use-context ${ctx}
-kubectx ${ctx}
-
 rm -f ${cacrt}
+
+# kubectl config use-context ${ctx}
+kubectl ctx ${ctx}
+kubectl config view --minify
