3c1a34dec73cd7a591c25732f90cbb726f3344a0
Julien Laurenceau <julienlaurenceau@gmail.com>
2025-07-23 12:14:40 +0200

    Update kubectl_ns_quota.sh

--- util/kubectl_ns_quota.sh
+++ util/kubectl_ns_quota.sh
@@ -3,6 +3,7 @@
 list=$(kubectl get ns -o custom-columns=NAME:.metadata.name --no-headers 2>/dev/null)
 
 for ns in $list ; do
+    echo "$ns:"
     cat <<EOF | kubectl apply -n $ns -f -
 apiVersion: v1
 kind: ResourceQuota
