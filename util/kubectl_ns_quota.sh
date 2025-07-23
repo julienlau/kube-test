7bd8d9a6d6b98db3f63229eb16f424743a55bb16
Julien Laurenceau <julienlaurenceau@gmail.com>
2025-07-23 12:10:58 +0200

    Create kubectl_ns_quota.sh

---
+++ util/kubectl_ns_quota.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+list=$(kubectl get ns -o custom-columns=NAME:.metadata.name --no-headers 2>/dev/null)
+
+for ns in $list ; do
+    cat <<EOF | kubectl apply -n $ns -f -
+apiVersion: v1
+kind: ResourceQuota
+metadata:
+  name: storage-quota
+spec:
+  hard:
+    requests.ephemeral-storage: 50Gi
+    limits.ephemeral-storage: 50Gi
+EOF
+done
